2af1b655b1d19388bab14fa0ae699102b39ad7b3
rmtsukuru <randolph.113@osu.edu>
2013-04-11 04:01:09 -0400

    Initial Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dendrites::Application.config.secret_token = 'a84b44dc73fd7ff8d50715d6a7f38056724e2c2d94abc28a88d34d6d855abe6a09d7f23dbe4d51874e907f291c64a25a16921b67d1a02edf461ddafe4b33e5b1'
